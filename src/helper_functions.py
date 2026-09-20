e44c443493f296433270193e13d41db857a9c767
Shayan Zarei <114865449+ZareiShayan@users.noreply.github.com>
2026-09-20 16:03:56 +0330

    Improve baseline handling in helper_functions.py
    Add handling for baseline tensor in helper function.

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -281,9 +281,14 @@ def compute_attribution_map(
     model = lit_model.model.to(device).eval()
 
     explain_x = explain_sample[0].to(device).unsqueeze(0)
-
+    
     if baseline is None:
         baseline = torch.zeros_like(explain_x)
+    else:
+        baseline = baseline.to(
+            device=device,
+            dtype=explain_x.dtype,
+        )
 
     def forward_fn(x):
         x = x.to(torch.float32)
